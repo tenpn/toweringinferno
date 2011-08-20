8b7af85a877c0811be14f040dfe7b72a0e46a855
andrew fray <andrew.fray@gmail.com>
2011-08-20 12:04:32 +0100

    initial procedural content, removed debug dir

---
+++ toweringinferno/proceduralgeneration/floorgenerator.h
@@ -0,0 +1,20 @@
+#ifndef __TI_FLOORGENERATOR_H_
+#define __TI_FLOORGENERATOR_H_
+
+namespace toweringinferno
+{
+	namespace proceduralgeneration
+	{
+
+class FloorGenerator
+{
+public:
+
+private:
+}; 
+
+	} // namespace proceduralgeneration
+
+} // namespace toweringinferno
+
+#endif // __TI_FLOORGENERATOR_H_
