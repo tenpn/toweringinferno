ec9f019b2c2c97d7543d66c9763d97e672397de6
andrew fray <andrew.fray@gmail.com>
2011-08-24 20:37:54 +0100

    heatvision initial files

---
+++ toweringinferno/heatvision/heatvisionsystem.h
@@ -0,0 +1,14 @@
+#ifndef __TI_HEATVISION_H_
+#define __TI_HEATVISION_H_
+
+namespace toweringinferno
+{
+namespace heatvision
+{
+
+
+
+} // namespace heatvision
+} // namespace toweringinferno
+
+#endif // __TI_HEATVISION_H_
