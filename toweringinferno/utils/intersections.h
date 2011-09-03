d0e49f3d5c32807b8ffa6ac94a32bc6fbc2fd521
andrew fray <andrew.fray@gmail.com>
2011-09-03 16:40:58 +0100

    new (empty) file for intersection math

---
+++ toweringinferno/utils/intersections.h
@@ -0,0 +1,14 @@
+#ifndef __TI_INTERSECTIONS_H_
+#define __TI_INTERSECTIONS_H_
+
+namespace toweringinferno
+{
+namespace utils
+{
+
+
+
+} // namespace utils
+} // namespace toweringinferno
+
+#endif // __TI_INTERSECTIONS_H_
