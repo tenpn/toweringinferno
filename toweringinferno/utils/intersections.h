164026ee10a5aabdcafeff0c48d0f6e3571f12bb
andrew fray <andrew.fray@gmail.com>
2011-09-03 20:35:46 +0100

    circle intersection object. is a point inside or outside the circle?

--- toweringinferno/utils/intersections.h
+++ toweringinferno/utils/intersections.h
@@ -1,12 +1,30 @@
 #ifndef __TI_INTERSECTIONS_H_
 #define __TI_INTERSECTIONS_H_
 
+#include "../position.h"
+
 namespace toweringinferno
 {
 namespace utils
 {
 
+class Circle
+{
+public:
+	Circle(const Point& origin, const int radius) : m_origin(origin), m_radius(radius) {}
 
+	bool contains(const Point& p)const;	
+
+private:
+	Point m_origin;
+	int m_radius;
+};
+
+inline
+bool toweringinferno::utils::Circle::contains(const Point& p)const
+{
+	return calculateManhattenDistance(p, m_origin) <= m_radius;
+}
 
 } // namespace utils
 } // namespace toweringinferno
