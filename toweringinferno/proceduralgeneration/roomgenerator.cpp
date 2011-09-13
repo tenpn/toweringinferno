ab27611aa1e2801d4f5a6feb907c5011c9c77993
andrew fray <andrew.fray@gmail.com>
2011-09-13 20:34:14 +0100

    added initial boardroom generation - just table for now

--- toweringinferno/proceduralgeneration/roomgenerator.cpp
+++ toweringinferno/proceduralgeneration/roomgenerator.cpp
@@ -45,12 +45,51 @@ void createDesk(
 	}
 }
 
+enum LineStyle
+{
+	eLineStyle_Single,
+	eLineStyle_Double,
+	eLineStyle_Count,
+};
+
+enum LineType
+{
+	eLineType_Horizontal,
+	eLineType_Vertical,
+	eLineType_Corner_NW,
+	eLineType_Corner_NE,
+	eLineType_Corner_SW,
+	eLineType_Corner_SE,
+	eLineType_Count,
+};
+
+static unsigned char s_lineConstants[eLineStyle_Count][eLineType_Count] =
+{
+	{
+		TCOD_CHAR_HLINE,
+		TCOD_CHAR_VLINE,
+		TCOD_CHAR_NW,
+		TCOD_CHAR_NE,
+		TCOD_CHAR_SW,
+		TCOD_CHAR_SE,
+	},
+	{
+		TCOD_CHAR_DHLINE,
+		TCOD_CHAR_DVLINE,
+		TCOD_CHAR_DNW,
+		TCOD_CHAR_DNE,
+		TCOD_CHAR_DSW,
+		TCOD_CHAR_DSE,
+	}
+};
+
 void ringBox(
 	const int x,
 	const int y,
 	const int w,
 	const int h,
-	FloorGenerator& floorOut
+	FloorGenerator& floorOut,
+	const LineStyle style
 	)
 {
 	const int maxX = x + w;
@@ -58,23 +97,37 @@ void ringBox(
 
 	for(int col = x; col < maxX; ++col)
 	{
-		floorOut.addFurnature(col, y, TCOD_CHAR_HLINE);
-		floorOut.addFurnature(col, maxY - 1, TCOD_CHAR_HLINE);
+		floorOut.addFurnature(col, y, s_lineConstants[style][eLineType_Horizontal]);
+		floorOut.addFurnature(col, maxY - 1, s_lineConstants[style][eLineType_Horizontal]);
 	}
 
 	for(int row = y; row < maxY; ++row)
 	{
-		floorOut.addFurnature(x, row, TCOD_CHAR_VLINE);
-		floorOut.addFurnature(maxX - 1, row, TCOD_CHAR_VLINE);
+		floorOut.addFurnature(x, row, s_lineConstants[style][eLineType_Vertical]);
+		floorOut.addFurnature(maxX - 1, row, s_lineConstants[style][eLineType_Vertical]);
 	}
 
-	floorOut.addFurnature(x,y, TCOD_CHAR_NW);
-	floorOut.addFurnature(x,maxY - 1, TCOD_CHAR_SW);
-	floorOut.addFurnature(maxX - 1,y, TCOD_CHAR_NE);
-	floorOut.addFurnature(maxX - 1,maxY - 1, TCOD_CHAR_SE);
+	floorOut.addFurnature(x,y, s_lineConstants[style][eLineType_Corner_NW]);
+	floorOut.addFurnature(x,maxY - 1, s_lineConstants[style][eLineType_Corner_SW]);
+	floorOut.addFurnature(maxX - 1,y, s_lineConstants[style][eLineType_Corner_NE]);
+	floorOut.addFurnature(maxX - 1,maxY - 1, s_lineConstants[style][eLineType_Corner_SE]);
 }
 
-class BSPDeskWriter : public ITCODBspCallback 
+void createBoardroom(
+	const int x,
+	const int y,
+	const int w,
+	const int h,
+	FloorGenerator& floorOut
+	)
+{
+	// draw table
+	ringBox(x+2, y+2, w-4, h-4, floorOut, eLineStyle_Double);
+
+	// surround with chairs
+}
+
+class SparseDesksWriter : public ITCODBspCallback 
 {
 public:
 	virtual bool visitNode(TCODBsp * node, void * userData)
@@ -104,9 +157,21 @@ void toweringinferno::proceduralgeneration::generateRoom(
 	FloorGenerator& floorOut
 	)
 {
-	TCODBsp deskBSP(x + 1, y + 1, w - 2, h - 2);
-	deskBSP.splitRecursive(&floorOut.getRNG(), 999, 5, 4, 0.95f, 0.95f);
+	// todo: cube farm
+	// todo: meeting room
+	// todo: store room
+	// todo: executive office
 
-	BSPDeskWriter deskWriter;
-	deskBSP.traversePostOrder(&deskWriter, &floorOut);
+	if (w > 8 && h > 8 && w < 13 && h < 13 && floorOut.getRNG().getInt(0,1) == 0)
+	{
+		createBoardroom(x, y, w, h, floorOut);
+	}
+	else
+	{
+		TCODBsp deskBSP(x + 1, y + 1, w - 2, h - 2);
+		deskBSP.splitRecursive(&floorOut.getRNG(), 999, 5, 4, 0.95f, 0.95f);
+
+		SparseDesksWriter deskWriter;
+		deskBSP.traversePostOrder(&deskWriter, &floorOut);
+	}
 }
