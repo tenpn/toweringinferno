33bc99ba098a2a727f17d8c70209de68d06af244
andrew fray <andrew.fray@gmail.com>
2011-09-13 11:34:39 +0100

    room generation files, init

---
+++ toweringinferno/proceduralgeneration/roomgenerator.cpp
@@ -0,0 +1,3 @@
+
+#include "roomgenerator.h"
+
---
+++ toweringinferno/proceduralgeneration/roomgenerator.h
@@ -0,0 +1,16 @@
+#ifndef __TI_ROOMGENERATOR_H_
+#define __TI_ROOMGENERATOR_H_
+
+namespace toweringinferno
+{
+	namespace proceduralgeneration
+	{
+
+class FloorGenerator;
+
+
+
+	}
+}
+
+#endif // __TI_ROOMGENERATOR_H_
